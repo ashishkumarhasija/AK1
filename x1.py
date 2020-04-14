57561ebb33da9fbd49eb9f82f39c1467505e33e4
ashishkumarhasija <50324428+ashishkumarhasija@users.noreply.github.com>
2020-04-14 14:04:41 +1000

    Add files via upload

---
+++ x1.py
@@ -0,0 +1,8 @@
+import os,re,shutil,glob,datetime,string
+print("=======================================================================" )
+#---- yyyymmdd format date ------
+x = datetime.datetime.now()
+date1=(x.strftime('%Y%m%d%H%M'))
+
+print(x)
+print(date1)
